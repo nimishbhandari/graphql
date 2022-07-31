7c334b708475b9fb0bbfdcd40ab29441d10445df
nimishbhandari <nimishbhandari727521@gmail.com>
2022-07-31 10:51:19 +0530

    db setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const connectDB = require("./config/db");
 const schema = require("./schema");
 require("dotenv").config();
 
 const PORT = process.env.PORT || PORT;
 
 const app = express();
+
+connectDB();
 
 app.use(
   "/graphql",
