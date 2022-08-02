d1ff742dba174834bb775f1dfa96e1f8a004735d
nimishbhandari <nimishbhandari727521@gmail.com>
2022-08-02 17:02:16 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import Client from "./component/Client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <ApolloProvider client={client}>
+        <div>
+          <h1>Hi</h1>
+          <Client />
+        </div>
+      </ApolloProvider>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/component/Client.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import ClientRow from "./ClientRow";
+
+const GET_CLIENT = gql`
+  query getClients {
+    clients {
+      id
+      name
+      email
+      phone
+    }
+  }
+`;
+
+const Client = () => {
+  const { loading, error, data } = useQuery(GET_CLIENT);
+
+  if (loading) return <p>Loading</p>;
+  if (error) return <p>Something went wrong</p>;
+
+  return (
+    <div>
+      {!loading && !error && (
+        <table className="table table-hover mt-3">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map((client) => (
+              <ClientRow key={client.id} client={client} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default Client;
